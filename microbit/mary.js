f8785a1800195c2441fab71fd59512891a27b001
Nicholas H.Tollervey <ntoll@ntoll.org>
2015-11-07 23:39:40 +0000

    Initial log entries for each device.

--- microbit/mary.js
+++ microbit/mary.js
@@ -1,11 +1,13 @@
 var mary_icon = new MicroIcon({iconUrl: '/img/mary.png'})
 
-var maryMicrobit = [{
-    popup: "Nicholas posted Mary micro:bit from here.",
-    lonLat: [52.133597, -0.990607],
-    options: {
-        icon: mary_icon
+var maryMicrobit = [
+    {
+        popup: "Nicholas posted Mary micro:bit from here.",
+        lonLat: [52.133597, -0.990607],
+        options: {
+            icon: mary_icon
+        }
     }
-}];
+];
 
 maryMicrobit.forEach(addMarker);
